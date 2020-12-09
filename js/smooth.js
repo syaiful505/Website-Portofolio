40f3d5ff4cd3c7b9623a18957db47d5e1e90b807
syaiful505 <syaifulstyle505@gmail.com>
2020-12-09 23:41:22 +0700

    menuju tujuan versi jqueri

--- js/smooth.js
+++ js/smooth.js
@@ -1,9 +1,13 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function(){
+$('.page-scroll').on('click', function(e){
 // cara membacanya jqueri $ tolong carikan saya () elemen berupa '.page-scrol' jika ketemu .on pada saat di klik ('click') maka , memberikan function (){ berupa}
     // ambill isi href this adalah pengambilan a yang di klik a dari isi href/tujuan yg dimaksud
     var tujuan = $(this).attr('href');
     // tangkap elemen ybs
     var elemenTujuan = $(tujuan);  // cara membacanya variabel elemenhref jqueri ambilkan saya tujuan yaitu href
-    console.log($('body').scrollTop()); // melihat nilai scroll top
+    // pindahkan scroll
+    $('body').scrollTop(elemenTujuan.offset().top);
+
+    e.preventDefault();
+    
 });
