c31398b23a6934753c9f64961db71054e8ab7ff1
syaiful505 <syaifulstyle505@gmail.com>
2020-12-09 23:31:31 +0700

    mengambil 1 elemen saat di klik

--- js/smooth.js
+++ js/smooth.js
@@ -1,7 +1,9 @@
 // event pada saat link di klik
 $('.page-scroll').on('click', function(){
 // cara membacanya jqueri $ tolong carikan saya () elemen berupa '.page-scrol' jika ketemu .on pada saat di klik ('click') maka , memberikan function (){ berupa}
-    // ambill isi href this adalah pengambilan a yang di klik a dari isi href 
-    var href = $(this).attr('href');
-    console.log(href);
+    // ambill isi href this adalah pengambilan a yang di klik a dari isi href/tujuan yg dimaksud
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan = $(tujuan);  // cara membacanya variabel elemenhref jqueri ambilkan saya tujuan yaitu href
+    console.log(elemenTujuan);
 });
