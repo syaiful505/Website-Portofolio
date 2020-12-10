42a399c14d126261d1eb74f34333a1b6cc963349
syaiful505 <syaifulstyle505@gmail.com>
2020-12-10 19:28:57 +0700

    sudah bisa smooth scroolling

--- js/smooth.js
+++ js/smooth.js
@@ -1,14 +1,14 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function(){
-// cara membacanya jqueri $ tolong carikan saya (
-) elemen berupa '.page-scrol' jika ketemu .on pada saat di klik ('click') maka , memberikan function (){ berupa}
+$('.page-scroll').on('click', function(e){
+// cara membacanya jqueri $ tolong carikan saya () elemen berupa '.page-scrol' jika ketemu .on pada saat di klik ('click') maka , memberikan function (){ berupa}
     // ambill isi href this adalah pengambilan a yang di klik a dari isi href/tujuan yg dimaksud
     var tujuan = $(this).attr('href');
     // tangkap elemen ybs
     var elemenTujuan = $(tujuan);  // cara membacanya variabel elemenhref jqueri ambilkan saya tujuan yaitu href
     // pindahkan scroll
-    $('body').animate({
+    $('html, body').animate({
         scrollTop: elemenTujuan.offset().top - 50
-    }, 1000);
-
+    }, 1200);
+    e.preventDefault();
+    
 });
