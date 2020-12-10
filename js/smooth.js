fbd5e5fb8e9aaf71028373fb8a0a967fed8e2291
syaiful505 <syaifulstyle505@gmail.com>
2020-12-10 19:36:53 +0700

    menambahkan jquery easing +animate

--- js/smooth.js
+++ js/smooth.js
@@ -8,7 +8,7 @@ $('.page-scroll').on('click', function(e){
     // pindahkan scroll
     $('html, body').animate({
         scrollTop: elemenTujuan.offset().top - 50
-    }, 1200);
+    }, 1250, 'easeInOutExpo');
     e.preventDefault();
     
 });
